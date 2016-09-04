6881573f6463236fc48dd76b1d935b2ee8e696f8
Jose Mauro Mendes Junior <josemauromendesjunior@yahoo.com.br>
2016-09-04 12:35:02 -0300

    jose mauro 04/09/2016 criada entidade instituição

--- src/java/asilar/model/entity/Instituicao.java
+++ src/java/asilar/model/entity/Instituicao.java
@@ -3,6 +3,95 @@ package asilar.model.entity;
 import asilar.model.entity.base.BaseEntity;
 
 public class Instituicao extends BaseEntity{
-    String cnpj;
-    String nome;
+    private String cnpj;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String rua;
+    private String numero;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String estado;
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+           
 }
