6eaed63bb78cc1b0f1042e39a5dce7f716c36606
Jose Mauro Mendes Junior <josemauromendesjunior@yahoo.com.br>
2016-09-07 09:50:18 -0300

    Jose Mauro 07/09/2016 Jose Mauro ritirando bug no usuariocriteria

--- src/java/asilar/controller/UsuarioController.java
+++ src/java/asilar/controller/UsuarioController.java
@@ -1,9 +1,12 @@
 package asilar.controller;
 
 import asilar.model.ServiceLocator;
+import asilar.model.criteria.UsuarioCriteria;
 import asilar.model.entity.Usuario;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +15,15 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UsuarioController {
-    
+
     @RequestMapping(value = "/cadastro/usuario/novo", method = RequestMethod.GET)
-    public ModelAndView create(){
+    public ModelAndView create() {
         ModelAndView mv = new ModelAndView("/cadastro/usuario/form");
         return mv;
     }
-    
+
     @RequestMapping(value = "/cadastro/usuario/novo", method = RequestMethod.POST)
-    public ModelAndView create(Usuario usuario){
+    public ModelAndView create(Usuario usuario) {
         try {
             ServiceLocator.getUsuarioService().create(usuario);
         } catch (Exception ex) {
@@ -29,9 +32,9 @@ public class UsuarioController {
         ModelAndView mv = new ModelAndView("redirect:/cadastro/usuario/lista");
         return mv;
     }
-    
+
     @RequestMapping(value = "/cadastro/usuario/{id}/info", method = RequestMethod.GET)
-    public ModelAndView readById(@PathVariable Long id){
+    public ModelAndView readById(@PathVariable Long id) {
         Usuario usuario = new Usuario();
         try {
             usuario = ServiceLocator.getUsuarioService().readyById(id);
@@ -42,61 +45,83 @@ public class UsuarioController {
         mv.addObject("usuario", usuario);
         return mv;
     }
-    
+
     @RequestMapping(value = "/cadastro/usuario/lista", method = RequestMethod.GET)
-    public ModelAndView readByCriteria(){
+    public ModelAndView readByCriteria(String nome, Long offset) {
+
+        boolean redirect = false;
+        if (offset == null || offset < 0) {
+            offset = 0L;
+            redirect = true;
+        }
+
         List<Usuario> usuarioList = new ArrayList<Usuario>();
+        Map<Long, Object> criteria = new HashMap<Long, Object>();
+        criteria.put(UsuarioCriteria.NOME_ILIKE, nome);
         Long count = 0L;
         try {
-            usuarioList = ServiceLocator.getUsuarioService().readyByCriteria(null, null);
-            count = ServiceLocator.getUsuarioService().countByCriteria(null);
+            usuarioList = ServiceLocator.getUsuarioService().readyByCriteria(criteria, offset);
+            count = ServiceLocator.getUsuarioService().countByCriteria(criteria);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        ModelAndView mv = new ModelAndView("/cadastro/usuario/lista");
-        mv.addObject("usuarioList", usuarioList);
-        mv.addObject("count", count);
-        String ativo = "usuario";
-        mv.addObject("ativo", ativo);
+        ModelAndView mv = null;
+
+        if (redirect) {
+            if (nome == null || nome.isEmpty()) {
+                mv = new ModelAndView("redirect:/cadastro/usuario/lista?offset=" + offset + "");
+            } else {
+                mv = new ModelAndView("redirect:/cadastro/usuario/lista?nome=" + nome + "&offset=" + offset + "");
+            }
+        } else {
+            mv = new ModelAndView("/cadastro/usuario/lista");
+            mv.addObject("usuarioList", usuarioList);
+            mv.addObject("count", count);
+            mv.addObject("offset", offset);
+            String ativo = "usuario";
+            mv.addObject("ativo", ativo);
+
+        }
+
         return mv;
     }
-    
-    @RequestMapping (value = "/cadastro/usuario/{id}/alterar", method = RequestMethod.GET)
-    public ModelAndView update(@PathVariable Long id){
+
+    @RequestMapping(value = "/cadastro/usuario/{id}/alterar", method = RequestMethod.GET)
+    public ModelAndView update(@PathVariable Long id) {
         Usuario usuario = new Usuario();
         try {
             usuario = ServiceLocator.getUsuarioService().readyById(id);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         ModelAndView mv = new ModelAndView("/cadastro/usuario/form");
         mv.addObject("usuario", usuario);
         return mv;
     }
-    
-    @RequestMapping (value = "/cadastro/usuario/{id}/alterar", method = RequestMethod.POST)
-    public ModelAndView update(Usuario usuario){
+
+    @RequestMapping(value = "/cadastro/usuario/{id}/alterar", method = RequestMethod.POST)
+    public ModelAndView update(Usuario usuario) {
         try {
             ServiceLocator.getUsuarioService().update(usuario);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         ModelAndView mv = new ModelAndView("redirect:/cadastro/usuario/lista");
         return mv;
     }
-    
-     @RequestMapping (value = "/cadastro/usuario/{id}/excluir", method = RequestMethod.GET)
-    public ModelAndView delete(@PathVariable Long id){
+
+    @RequestMapping(value = "/cadastro/usuario/{id}/excluir", method = RequestMethod.GET)
+    public ModelAndView delete(@PathVariable Long id) {
         try {
             ServiceLocator.getUsuarioService().delete(id);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         ModelAndView mv = new ModelAndView("redirect:/cadastro/usuario/lista");
         return mv;
     }
-    
+
 }
